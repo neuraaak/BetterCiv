9427dfccd6b6848de66eaa0aeb43b6b34f116c4b
Neuraaak <floriansalort@outlook.fr>
2025-07-30 20:38:56 +0200

    feat(config): réorganiser les configurations du projet v1.1.0

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,16 +1,33 @@
 import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
 
 export default [
     js.configs.recommended,
     {
-        files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+        files: ['**/*.vue'],
+        plugins: {
+            vue,
+        },
         languageOptions: {
             ecmaVersion: 2021,
-            sourceType: 'module'
+            sourceType: 'module',
+            parser: vue.parser,
+        },
+        rules: {
+            ...vue.configs.recommended.rules,
+            'no-console': 'off',
+            'no-debugger': 'off',
+        },
+    },
+    {
+        files: ['**/*.js', '**/*.jsx'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
         },
         rules: {
             'no-console': 'off',
-            'no-debugger': 'off'
-        }
-    }
-]; 
+            'no-debugger': 'off',
+        },
+    },
+];
